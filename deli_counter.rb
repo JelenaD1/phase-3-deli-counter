a7a69cb23fdea4e47de3fce33bb24b4425ec8b8d
Jelena <jelena.durovic@powerhrg.com>
2021-08-08 19:23:42 -0400

    commit tree 3272049fe2856a1ce3836304cddf06ae3dcc6e83

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+
+def line(array)
+    if array.empty? 
+      puts  "The line is currently empty."
+    else 
+       message = array.map.with_index {|name, index| "#{index +1}. #{name}"}.join(" ")
+        puts "The line is currently: #{message}"
+    end 
+end 
+ 
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+
+end 
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+    puts "Currently serving #{array[0]}."
+
+    array.shift
+    end 
+    
+end 
